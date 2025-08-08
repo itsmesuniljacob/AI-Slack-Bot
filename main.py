194a760e580c39ada545a0ca0ce8a1efa5053649
Sunil Jacob <sunil1405@gmail.com>
2025-08-08 17:30:04 +0530

    AI-bot-receiver-lambda

---
+++ main.py
@@ -0,0 +1,30 @@
+import json
+import base64
+from urllib.parse import unquote_plus
+
+def lambda_handler(event, context):
+    print(event)
+    message = (base64.b64decode(event['body']).decode('utf-8').split('text=')[1]).split('&')[0]
+    body = base64.b64decode(event['body']).decode('utf-8')
+    print("Message: " + unquote_plus(message))
+
+    try:
+    
+        # Handle Slack URL verification challenge
+        if 'challenge' in body:
+            return {
+                'statusCode': 200,
+                'body': body.split('challenge=')[1].split('&')[0]
+            }
+            
+
+        return {
+            'statusCode': 200,
+            'body': json.dumps('Hello from Lambda!')
+        }
+    except Exception as e:
+        print(e)
+        return {
+            'statusCode': 500,
+            'body': json.dumps('Error!')
+        }
